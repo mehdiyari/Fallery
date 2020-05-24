a7cc5a77c59e673352e0ee3e3f3343d3fc47df39
Mehdi Yari <mehdi.email.info@gmail.com>
2020-05-24 16:13:32 +0430

    add two utility extension functions for checking permission inside Activities and Fragments

---
+++ fallery/src/main/java/ir/mehdiyari/fallery/utils/PermissionHelper.kt
@@ -0,0 +1,37 @@
+package ir.mehdiyari.fallery.utils
+
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+
+
+inline fun AppCompatActivity.permissionChecker(
+    permission: String,
+    granted: () -> Unit = {},
+    denied: () -> Unit = {}
+) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+       if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            denied()
+        } else {
+            granted()
+        }
+    } else {
+        granted()
+    }
+}
+
+inline fun FragmentActivity.permissionChecker(
+    permission: String, granted: () -> Unit, denied: () -> Unit
+) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            denied()
+        } else {
+            granted()
+        }
+    } else {
+        granted()
+    }
+}
